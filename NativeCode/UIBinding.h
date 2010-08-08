7456473ba6928a35eb2b9b2c218982dc76fc14ed
uprise78 <desaro@gmail.com>
2010-08-07 18:31:43 -0700

    Comment update

--- NativeCode/UIBinding.h
+++ NativeCode/UIBinding.h
@@ -1,12 +1,9 @@
 //
 //  UIBinding.h
 //  Unity-iPhone
-//
-//  Created by Gregg Patton on 7/6/10.
-//  Copyright 2010 Gregg Patton. All rights reserved.
-//
 
 #import <Foundation/Foundation.h>
+
 
 void _activateUIWithController( const char *controllerName );
 
