83aa751d9d497611de2a87041b0ab6322a0908e9
uprise78 <desaro@gmail.com>
2011-02-20 15:10:38 -0800

    added stringextensions helper

---
+++ Scripts/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+
+public static class StringExtensions
+{
+	public static string pathForBundleResource( string file )
+	{
+		var path = Application.dataPath.Replace( "Data", "" );
+		return System.IO.Path.Combine( path, file );
+	}
+
+
+	public static string pathForBundleResource( string file )
+	{
+		return System.IO.Path.Combine( Environment.GetFolderPath( Environment.SpecialFolder.Personal ), file );
+	}
+}
