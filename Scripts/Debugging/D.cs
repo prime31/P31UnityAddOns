0937cba195d2e7cf41378dd9406819d73058c65e
Mike <desaro@gmail.com>
2011-10-12 15:55:47 -0700

    updated D class for video added git ignore base

--- Scripts/Debugging/D.cs
+++ Scripts/Debugging/D.cs
@@ -1,25 +1,34 @@
+#define DEBUG_LEVEL_LOG
+#define DEBUG_LEVEL_WARN
+#define DEBUG_LEVEL_ERROR
+
+
 using UnityEngine;
 using System.Collections;
 
 
 // setting the conditional to the platform of choice will only compile the method for that platform
+// alternatively, use the #defines at the top of this file
 public class D
 {
-	[System.Diagnostics.Conditional( "UNITY_EDITOR" )]
+	[System.Diagnostics.Conditional( "DEBUG_LEVEL_LOG" )]
+	[System.Diagnostics.Conditional( "DEBUG_LEVEL_WARN" )]
+	[System.Diagnostics.Conditional( "DEBUG_LEVEL_ERROR" )]
 	public static void log( string format, params object[] paramList )
 	{
 		Debug.Log( string.Format( format, paramList ) );
 	}
 	
 	
-	[System.Diagnostics.Conditional( "UNITY_EDITOR" )]
+	[System.Diagnostics.Conditional( "DEBUG_LEVEL_WARN" )]
+	[System.Diagnostics.Conditional( "DEBUG_LEVEL_ERROR" )]
 	public static void warn( string format, params object[] paramList )
 	{
 		Debug.LogWarning( string.Format( format, paramList ) );
 	}
 	
 	
-	[System.Diagnostics.Conditional( "UNITY_EDITOR" )]
+	[System.Diagnostics.Conditional( "DEBUG_LEVEL_ERROR" )]
 	public static void error( string format, params object[] paramList )
 	{
 		Debug.LogError( string.Format( format, paramList ) );
@@ -27,31 +36,32 @@ public class D
 	
 	
 	[System.Diagnostics.Conditional( "UNITY_EDITOR" )]
+	[System.Diagnostics.Conditional( "DEBUG_LEVEL_LOG" )]
 	public static void assert( bool condition )
 	{
-		assert( condition, string.Empty );
+		assert( condition, string.Empty, true );
 	}
-	
+
 	
 	[System.Diagnostics.Conditional( "UNITY_EDITOR" )]
+	[System.Diagnostics.Conditional( "DEBUG_LEVEL_LOG" )]
 	public static void assert( bool condition, string assertString )
 	{
-		if( !condition )
-			Debug.LogError( "assert failed! " + assertString );
+		assert( condition, assertString, false );
 	}
-	
 
-	[System.Diagnostics.Conditional( "UNITY_EDITOR" )]
-	public static void assertFail()
-	{
-		assertFail( string.Empty );
-	}
-	
 	
 	[System.Diagnostics.Conditional( "UNITY_EDITOR" )]
-	public static void assertFail( string assertString )
+	[System.Diagnostics.Conditional( "DEBUG_LEVEL_LOG" )]
+	public static void assert( bool condition, string assertString, bool pauseOnFail )
 	{
-		Debug.LogError( assertString );
+		if( !condition )
+		{
+			Debug.LogError( "assert failed! " + assertString );
+			
+			if( pauseOnFail )
+				Debug.Break();
+		}
 	}
 
 }
